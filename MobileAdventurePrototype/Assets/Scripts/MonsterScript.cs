e11a6da1af2ce6b75d83e187094dc2ed1955574a
Omer Ahmed <omera407@gmail.com>
2022-06-10 20:17:35 -0600

    Created Unity project, created relatively empty script files, added placeholder assets

---
+++ MobileAdventurePrototype/Assets/Scripts/MonsterScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScript : MonoBehaviour
+{
+    Vector2 destinationPosition;
+    AdventurerScript currentTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
